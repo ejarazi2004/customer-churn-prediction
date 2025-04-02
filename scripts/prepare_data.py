f76926cda90a199be9a1dfa515fd799a180880e5
ejarazi2004 <115077709+ejarazi2004@users.noreply.github.com>
2025-04-02 19:36:07 +0200

    Function to load, split, use preproc pipeline

---
+++ scripts/prepare_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from split_data import load_split_data
+from preprocess_data import build_preproc_pipeline
+
+def prepare_data(data_path):
+    #Loading the split datasets
+    X_train, X_test, y_train, y_test = load_split_data(data_path)
+    
+    #Pipline building
+    preprocessor = build_preproc_pipeline()
+    
+    #Fitting the pipeline only in the train data
+    preprocessor.fit(X_train)
+    
+    #Transforming both train and test
+    X_train_preprocessed = preprocessor.transform(X_train)
+    X_test_preprocessed = preprocessor.transform(X_test)
+    
+    return X_train_preprocessed, X_test_preprocessed, y_train, y_test
+
+#If script is run once through the terminal
+if __name__ == "__main__":
+    DATA_PATH = "../data/raw/Bank Customer Churn Prediction.csv"
+    
+    X_train_p, X_test_p, y_train, y_test = prepare_data(DATA_PATH)
+    
+    print("Train transformed shape:", X_train_p.shape)
+    print("Test transformed shape: ", X_test_p.shape)
